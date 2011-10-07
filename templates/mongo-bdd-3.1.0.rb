f9e92c3485db4a36769081785e2b878d7c4f5e35
Robert Brewitz <hello@robertbrewitz.com>
2011-10-07 18:20:23 +0200

    Updated template and README, including test suite configuration in template file

--- templates/mongo-bdd-3.1.0.rb
+++ templates/mongo-bdd-3.1.0.rb
@@ -57,6 +57,7 @@ git :add => "."
 git :commit => "-m \"Initial commit\""
 
 run "bundle install"
+run "rails g mongoid:config"
 run "rails g rspec:install"
 run "mkdir spec/support spec/models spec/routing"
 run "touch spec/support/.gitkeep spec/models/.gitkeep spec/routing/.gitkeep"
@@ -64,10 +65,75 @@ run "guard init rspec"
 run "spork --bootstrap"
 run "guard init spork"
 
-git :add => "."
-git :commit => "-m \"Test suite installed\""
+file "Guardfile", <<-END
+guard 'spork', :cucumber_env => { 'RAILS_ENV' => 'test' }, :rspec_env => { 'RAILS_ENV' => 'test' } do
+  watch('config/application.rb')
+  watch('config/environment.rb')
+  watch(%r{^config/environments/.+\.rb$})
+  watch(%r{^config/initializers/.+\.rb$})
+  watch('Gemfile')
+  watch('Gemfile.lock')
+  watch('spec/spec_helper.rb')
+  watch('test/test_helper.rb')
+  watch(%r{^spec/support/.+\.rb$})
+end
 
-run "rails generate mongoid:config"
+guard 'rspec', :version => 2, :cli => "--drb" do
+  watch(%r{^spec/.+_spec\.rb$})
+  watch(%r{^lib/(.+)\.rb$})     { |m| "spec/lib/\#{m[1]}_spec.rb" }
+  watch('spec/spec_helper.rb')  { "spec" }
+
+  # Rails example
+  watch(%r{^spec/.+_spec\.rb$})
+  watch(%r{^app/(.+)\.rb$})                           { |m| "spec/\#{m[1]}_spec.rb" }
+  watch(%r{^lib/(.+)\.rb$})                           { |m| "spec/lib/\#{m[1]}_spec.rb" }
+  watch(%r{^app/controllers/(.+)_(controller)\.rb$})  { |m| ["spec/routing/\#{m[1]}_routing_spec.rb", "spec/\#{m[2]}s/\#{m[1]}_\#{m[2]}_spec.rb", "spec/acceptance/\#{m[1]}_spec.rb"] }
+  watch(%r{^spec/support/(.+)\.rb$})                  { "spec" }
+  watch('spec/spec_helper.rb')                        { "spec" }
+  watch('config/routes.rb')                           { "spec/routing" }
+  watch('app/controllers/application_controller.rb')  { "spec/controllers" }
+  # Capybara request specs
+  watch(%r{^app/views/(.+)/.*\.(erb|haml)$})          { |m| "spec/requests/\#{m[1]}_spec.rb" }
+end
+END
+
+file "spec/spec_helper.rb", <<-END
+require 'rubygems'
+require 'spork'
+
+Spork.prefork do
+  ENV["RAILS_ENV"] ||= 'test'
+  require File.expand_path("../../config/environment", __FILE__)
+  require 'rspec/rails'
+  require 'capybara/rspec'
+  require 'database_cleaner'
+  require "rails/mongoid"
+
+  Spork.trap_class_method(Rails::Mongoid, :load_models)
+  Spork.trap_method(Rails::Application::RoutesReloader, :reload!)
+
+  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
+  RSpec.configure do |config|
+    config.mock_with :rspec
+
+    config.before(:suite) do
+      DatabaseCleaner.strategy = :truncation
+      DatabaseCleaner.orm = "mongoid"
+    end
+
+    config.before(:each) do
+      DatabaseCleaner.clean
+    end
+
+    config.include Mongoid::Matchers
+  end
+end
+
+Spork.each_run do
+  FactoryGirl.reload
+end
+END
 
 git :add => "."
-git :commit => "-m \"Mongoid installed\""
+git :commit => "-m \"Development and test suite set up\""
