6eb9c8f04be7038e8bababc7f35c4fe1e001a205
Robert Brewitz <hello@robertbrewitz.com>
2013-02-14 23:54:05 +0100

    Updated mongo-tdd template.

--- rails/templates/mongo-tdd.rb
+++ rails/templates/mongo-tdd.rb
@@ -5,37 +5,48 @@ run "rm public/index.html app/assets/images/rails.png"
 file "Gemfile", <<-END
 source 'http://rubygems.org'
 
-gem 'rails', '3.1.1'
+gem 'rails', '3.2.12'
 
-gem 'mongoid', '~> 2.3.3'
-gem 'bson_ext', '~> 1.3.1'
-gem 'mongo', '~> 1.3.1'
-gem 'bson', '~> 1.3.1'
+##
+# Database
+gem 'mongoid', '3.1.0'
+gem 'paperclip', '3.1.4'
+gem 'mongoid-paperclip', '0.0.7', require: 'mongoid_paperclip'
+
+gem 'haml', '3.1.6'
+gem 'simple_form', '2.0.2'
 
 group :assets do
-  gem 'sass-rails', '  ~> 3.1.4'
-  gem 'coffee-rails', '~> 3.1.1'
-  gem 'haml', '~> 3.1.3'
-  gem 'haml-rails', '~> 0.3.4'
-  gem 'uglifier'
+  gem 'coffee-rails', '3.2.2'
+  gem 'uglifier', '1.3.0'
+  gem 'jquery-rails', '2.2.1'
+  gem 'less', '2.2.2'
+  gem 'less-rails', '2.2.6'
 end
 
-gem 'jquery-rails'
+group :development do
+  gem 'asset_sync', '0.5.4'
+  gem 'ruby-debug19', '0.11.6'
+  gem 'therubyracer', '0.11.3', platforms: :ruby
+end
 
-gem 'rspec-rails', :group => [:test, :development]
 group :test do
   gem 'factory_girl_rails'
   gem 'capybara'
+  gem 'rspec-rails'
   gem 'guard-rspec'
   gem 'mongoid-rspec'
   gem 'database_cleaner'
-  gem 'spork', '~> 0.9.0.rc9'
+  gem 'spork'
   gem 'guard-spork'
+  gem 'cucumber-rails', :require => false
+  gem 'guard-cucumber'
 end
+
 END
 
 file "LICENSE", <<-END
-Copyright (c) 2011 Your Name
+Copyright (c) 2013 Your Name
 
 Permission is hereby granted, free of charge, to any person obtaining
 a copy of this software and associated documentation files (the
@@ -70,28 +81,45 @@ run "spork --bootstrap"
 run "guard init spork"
 
 file "Guardfile", <<-END
-guard 'spork', :rspec_env => { 'RAILS_ENV' => 'test' } do
+guard 'cucumber' do
+  watch(%r{^features/.+\.feature$})
+  watch(%r{^features/support/.+$})          { 'features' }
+  watch(%r{^features/step_definitions/(.+)_steps\.rb$}) { |m| Dir[File.join("**/\#{m[1]}.feature")][0] || 'features' }
+end
+
+guard 'rspec', :version => 2 do
+  watch(%r{^spec/.+_spec\.rb$})
+  watch(%r{^lib/(.+)\.rb$})     { |m| "spec/lib/\#{m[1]}_spec.rb" }
+  watch('spec/spec_helper.rb')  { "spec" }
+
+  # Rails example
+  watch(%r{^app/(.+)\.rb$})                           { |m| "spec/\#{m[1]}_spec.rb" }
+  watch(%r{^app/(.*)(\.erb|\.haml)$})                 { |m| "spec/\#{m[1]}\#{m[2]}_spec.rb" }
+  watch(%r{^app/controllers/(.+)_(controller)\.rb$})  { |m| ["spec/routing/\#{m[1]}_routing_spec.rb", "spec/\#{m[2]}s/\#{m[1]}_\#{m[2]}_spec.rb", "spec/acceptance/\#{m[1]}_spec.rb"] }
+  watch(%r{^spec/support/(.+)\.rb$})                  { "spec" }
+  watch('config/routes.rb')                           { "spec/routing" }
+  watch('app/controllers/application_controller.rb')  { "spec/controllers" }
+  
+  # Capybara request specs
+  watch(%r{^app/views/(.+)/.*\.(erb|haml)$})          { |m| "spec/requests/\#{m[1]}_spec.rb" }
+  
+  # Turnip features and steps
+  watch(%r{^spec/acceptance/(.+)\.feature$})
+  watch(%r{^spec/acceptance/steps/(.+)_steps\.rb$})   { |m| Dir[File.join("**/\#{m[1]}.feature")][0] || 'spec/acceptance' }
+end
+
+guard 'spork', :cucumber_env => { 'RAILS_ENV' => 'test' }, :rspec_env => { 'RAILS_ENV' => 'test' } do
   watch('config/application.rb')
   watch('config/environment.rb')
-  watch(%r{^config/environments/.+\.rb$})
+  watch('config/environments/test.rb')
   watch(%r{^config/initializers/.+\.rb$})
   watch('Gemfile')
   watch('Gemfile.lock')
-  watch('spec/spec_helper.rb')
-  watch(%r{^spec/support/.+\.rb$})
+  watch('spec/spec_helper.rb') { :rspec }
+  watch('test/test_helper.rb') { :test_unit }
+  watch(%r{features/support/}) { :cucumber }
 end
 
-guard 'rspec', :version => 2, :cli => "--drb" do
-  watch(%r{^spec/.+_spec\.rb$})
-  watch(%r{^app/(.+)\.rb$})                           { |m| "spec/\#{m[1]}_spec.rb" }
-  watch(%r{^lib/(.+)\.rb$})                           { |m| "spec/lib/\#{m[1]}_spec.rb" }
-  watch(%r{^app/controllers/(.+)_(controller)\.rb$})  { |m| ["spec/routing/\#{m[1]}_routing_spec.rb", "spec/\#{m[2]}s/\#{m[1]}_\#{m[2]}_spec.rb", "spec/acceptance/\#{m[1]}_spec.rb"] }
-  watch(%r{^spec/support/(.+)\.rb$})                  { "spec" }
-  watch('spec/spec_helper.rb')                        { "spec" }
-  watch('config/routes.rb')                           { "spec/routing" }
-  watch('app/controllers/application_controller.rb')  { "spec/controllers" }
-  watch(%r{^app/views/(.+)/.*\.(erb|haml)$})          { |m| "spec/requests/\#{m[1]}_spec.rb" }
-end
 END
 
 file "spec/spec_helper.rb", <<-END
@@ -102,34 +130,38 @@ Spork.prefork do
   ENV["RAILS_ENV"] ||= 'test'
   require File.expand_path("../../config/environment", __FILE__)
   require 'rspec/rails'
-  require 'capybara/rspec'
+  require 'capybara/rails'
   require 'database_cleaner'
   require 'rails/mongoid'
-
+  require File.dirname(__FILE__) + "/support/controller_macros"
+  
   Spork.trap_class_method(Rails::Mongoid, :load_models)
   Spork.trap_method(Rails::Application::RoutesReloader, :reload!)
-
-  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
-
+  
+  Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+  
   RSpec.configure do |config|
     config.mock_with :rspec
-
+    
     config.before(:suite) do
+      DatabaseCleaner.orm = "mongoid"
       DatabaseCleaner.strategy = :truncation
-      DatabaseCleaner.orm = 'mongoid'
     end
-
+    
     config.before(:each) do
       DatabaseCleaner.clean
     end
-
+    
     config.include Mongoid::Matchers
+    config.include Devise::TestHelpers, type: :controller
+    config.include ControllerMacros, type: :controller
   end
 end
 
 Spork.each_run do
   FactoryGirl.reload
 end
+
 END
 
 git :add => "."
